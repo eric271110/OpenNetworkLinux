738c662fc2d52e7c5164a4ff4e973caa0e379336
Eric Yang <eric_yang@accton.com>
2026-03-13 11:52:54 +0800

    add new platform

---
+++ packages/platforms/accton/x86-64/as1813-128/src/x86_64_accton_as1813_128/module/auto/make.mk
@@ -0,0 +1,8 @@
+###############################################################################
+#
+# x86_64_accton_as1813_128 Autogeneration
+#
+###############################################################################
+x86_64_accton_as1813_128_AUTO_DEFS := module/auto/x86_64_accton_as1813_128.yml
+x86_64_accton_as1813_128_AUTO_DIRS := module/inc/x86_64_accton_as1813_128 module/src
+include $(BUILDER)/auto.mk
---
+++ packages/platforms/accton/x86-64/as1813-128/src/x86_64_accton_as1813_128/module/inc/x86_64_accton_as1813_128/x86_64_accton_as1813_128_dox.h
@@ -0,0 +1,26 @@
+/**************************************************************************//**
+ *
+ * x86_64_accton_as1813_128 Doxygen Header
+ *
+ *****************************************************************************/
+#ifndef __x86_64_accton_as1813_128_DOX_H__
+#define __x86_64_accton_as1813_128_DOX_H__
+
+/**
+ * @defgroup x86_64_accton_as1813_128 x86_64_accton_as1813_128 - x86_64_accton_as1813_128 Description
+ *
+
+The documentation overview for this module should go here.
+
+ *
+ * @{
+ *
+ * @defgroup x86_64_accton_as1813_128-x86_64_accton_as1813_128 Public Interface
+ * @defgroup x86_64_accton_as1813_128-config Compile Time Configuration
+ * @defgroup x86_64_accton_as1813_128-porting Porting Macros
+ *
+ * @}
+ *
+ */
+
+#endif /* __x86_64_accton_as1813_128_DOX_H__ */
