d11d814090929b9b75633de119ab305d45beb8b2
Eric Yang <eric_yang@accton.com>
2026-04-30 17:06:11 +0800

    [Accton][as1813-128o] Add new platform

--- packages/base/all/vendor-config-onl/src/python/onl/platform/base.py
+++ packages/base/all/vendor-config-onl/src/python/onl/platform/base.py
@@ -650,3 +650,6 @@ class OnlPlatformPortConfig_40x100_13x400_2x10(object):
 class OnlPlatformPortConfig_64x800_2x25(object):
     PORT_COUNT=66
     PORT_CONFIG="64x800 + 2x25"
+class OnlPlatformPortConfig_128x800_2x25(object):
+    PORT_COUNT=130
+    PORT_CONFIG="128x800 + 2x25"
